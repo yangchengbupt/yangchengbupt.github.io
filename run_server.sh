f707d977fa51c86e710d749b5d9bf53514334fc9
lixin <lixin4sky@gmail.com>
2025-01-13 16:36:58 +0800

    selected pubs

--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,2 @@
 # bundle exec jekyll liveserve
-bundle exec jekyll serve --host 127.0.0.1 --port 5000 --watch --livereload
+bundle exec jekyll serve --host 127.0.0.1 --port 4000 --watch --livereload
